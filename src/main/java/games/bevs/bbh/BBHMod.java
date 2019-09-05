16007822663cb87e957d8a4b3019104637d817b5
Heath Logan Campbell <github@heathlogancampbell.com>
2019-09-05 20:48:25 +1200

    init project

---
+++ src/main/java/games/bevs/bbh/BBHMod.java
@@ -0,0 +1,19 @@
+package games.bevs.bbh;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = BBHMod.MODID, version = BBHMod.VERSION)
+public class BBHMod
+{
+    public static final String MODID = "bbhmod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+}
