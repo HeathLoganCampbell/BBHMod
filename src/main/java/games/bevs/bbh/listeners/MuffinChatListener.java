f7a5d07d0b27aa7b6543e2333ddb383440ff9d59
Heath Logan Campbell <github@heathlogancampbell.com>
2019-09-05 21:35:23 +1200

    improve filter

--- src/main/java/games/bevs/bbh/listeners/MuffinChatListener.java
+++ src/main/java/games/bevs/bbh/listeners/MuffinChatListener.java
@@ -3,12 +3,14 @@ package games.bevs.bbh.listeners;
 import games.bevs.bbh.utils.BadWordUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.fml.client.config.GuiConfigEntries.ChatColorEntry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class MuffinChatListener 
 {
-	private static final String REPLACEMENT_WORD = "muffin";
+	private static final String REPLACEMENT_WORD = EnumChatFormatting.BOLD + "muffin" + EnumChatFormatting.RESET;
 	
 	private String filterBadWords(String statement)
 	{
@@ -19,9 +21,15 @@ public class MuffinChatListener
 		
 		for(String word : words)
 		{
-			if(BadWordUtils.isBad(word))
+			String unformattedWord = word;
+			
+			for(EnumChatFormatting chatColor : EnumChatFormatting.values())
+				unformattedWord = word.replaceAll(chatColor.toString(), "");
+			
+			if(BadWordUtils.isBad(unformattedWord))
 				isBad = true;
-			strBuilder.append(BadWordUtils.isBad(word) 
+			
+			strBuilder.append(isBad
 								? REPLACEMENT_WORD 
 								: word);
 			strBuilder.append(" ");
@@ -37,10 +45,14 @@ public class MuffinChatListener
 	public void onChat(ClientChatReceivedEvent e) 
 	{
 		String formated = e.message.getFormattedText();
+		String unformated = e.message.getUnformattedText();
 		
 		formated = this.filterBadWords(formated);
 		
 		ChatComponentText chatComponentText = new ChatComponentText(formated);
+		
 		Minecraft.getMinecraft().thePlayer.addChatMessage(chatComponentText);
+		
+		e.setCanceled(true);
 	}
 }
